1b12911298119d3c85d51957a6b205392e26f774
Micooz <micooz@hotmail.com>
2015-08-22 13:25:41 +0800

    Remove compressjs.sh.

--- compressjs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-jsfiles=`ls public/js/ | grep ^[a-z]*.js$`
-
-for js in $jsfiles; do
-  name=`echo ${js} | cut -d "." -f1`
-  uglifyjs public/js/${js} -c -m -o public/js/${name}.min.js --lint --comments
-done
-
